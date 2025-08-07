ff15565476d1de202ef5915c610d4255df2b6487
Alex Pacheco <alexrodpacheco@gmail.com>
2025-08-07 14:57:37 -0300

    first commit

---
+++ TapAppGame/TapAppGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapAppGame
+//
+//  Created by Alex Rodrigues Pacheco on 07/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapAppGame/TapAppGame/TapAppGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapAppGameApp.swift
+//  TapAppGame
+//
+//  Created by Alex Rodrigues Pacheco on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TapAppGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
